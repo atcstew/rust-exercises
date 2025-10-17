66ab3a1a664ad5cbc2913a6a315d3e33971612f2
Aidan Stewart <astewart@tektelic.com>
2025-10-17 13:03:47 -0600

    Finish exercise 2
    Signed-off-by: Aidan Stewart <astewart@tektelic.com>

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,15 @@ fn exercise1() {
     println!("Hello, {}, nice to meet you!", name.trim());
 }
 
+fn exercise2() {
+    println!("What is the input string? ");
+    let mut word = String::new();
+    io::stdin().read_line(&mut word).expect("Failed to get input");
+    let count = word.trim().len();
+    println!("{} has {count} characters.", word.trim());
+}
+
 fn main() {
-    exercise1();
+    //exercise1();
+    exercise2();
 }
