c62463a4060cb0624580b45e8fe8b83382ebf512
Aidan Stewart <astewart@tektelic.com>
2025-10-17 13:19:37 -0600

    Finish exercise 3
    Signed-off-by: Aidan Stewart <astewart@tektelic.com>

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,26 @@ fn exercise2() {
     println!("{} has {count} characters.", word.trim());
 }
 
+fn exercise3() {
+    println!("What is the quote? ");
+    let mut quote = String::new();
+    let mut author = String::new();
+
+    io::stdin().read_line(&mut quote).expect("Failed to get input");
+    quote = quote.trim().to_string();
+    println!("Who said it? ");
+    io::stdin().read_line(&mut author).expect("Failed to get input");
+    author = author.trim().to_string();
+    println!("{author} says \"{quote}\".");
+}
+
+fn exercise4() {
+
+}
+
 fn main() {
     //exercise1();
-    exercise2();
+    //exercise2();
+    //exercise3();
+    exercise4();
 }
