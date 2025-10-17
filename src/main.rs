43a686cd2809535ee3a8eda4f6287e1089614de4
Aidan Stewart <astewart@tektelic.com>
2025-10-17 12:50:16 -0600

    Finish exercise 1
    Signed-off-by: Aidan Stewart <astewart@tektelic.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
+fn exercise1() {
+    println!("What is your name? ");
+    let mut name = String::new();
+    io::stdin().read_line(&mut name).expect("Failed to get input");
+    println!("Hello, {}, nice to meet you!", name.trim());
+}
+
 fn main() {
-    println!("Hello, world!");
+    exercise1();
 }
